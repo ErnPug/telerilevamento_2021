f1b41143cf5124ca8bed5ab03a8fb7ebced7e794
ErnPug <80513465+ErnPug@users.noreply.github.com>
2021-03-17 17:56:45 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -10,9 +10,11 @@ p224r63_2011
 plot(p224r63_2011)
 
 # cambio i colori dei plot
-color <- colorRampPalette(c("black","grey","light grey"))(100)
+color <- colorRampPalette(c("black","grey","light grey"))(100) 
 
 plot(p224r63_2011,col=color)
 
-color <- colorRampPalette(c("blue","light blu","green","yellow","red"))(100)
+color <- colorRampPalette(c("blue","light blu","green","yellow","red"))(100) # cambio i colori
+
+plot(p224r63_2011,col=color)
 
