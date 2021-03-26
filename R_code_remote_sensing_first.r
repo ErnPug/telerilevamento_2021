77b49c74f3ae7347f373723ab391b58bffc0196d
ErnPug <80513465+ErnPug@users.noreply.github.com>
2021-03-26 14:31:12 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -9,6 +9,7 @@ p224r63_2011
 
 plot(p224r63_2011)
 
+# Day 2
 # cambio i colori dei plot
 color <- colorRampPalette(c("black","grey","light grey"))(100) 
 
@@ -17,4 +18,46 @@ plot(p224r63_2011,col=color)
 color <- colorRampPalette(c("blue","light blue","green","yellow","red"))(100) # cambio i colori
 
 plot(p224r63_2011,col=color)
+
+# Day 3
+# Bande Landsat
+# B1: blu
+# B2: verde
+# B3: rosso
+# B4: infrarosso vicino
+# B5: infrarosso medio
+# B6: infrarosso termico
+# B7: infrarosso medio
+
+# Cancello i plot precedente
+dev.off()
+
+# plotto solo la banda 1. $lega la banda all'immagine
+plot(p224r63_2011$B1_sre)
+# cambio la color palette della banda 1
+dev.off()
+color <- colorRampPalette(c("blue","light blue","green","yellow","red"))(100)
+plot(p224r63_2011$B1_sre,col=color)
+# uso la funzione "par" per poter devidere come plottare. Plotto la banda del blu vicino alla banda del verde
+par(mfrow=c(1,2))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+# plotto le prime 4 bande cambiando per ognuno la palette di colori
+par(mfrow=c(4,1))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre,)
+plot(p224r63_2011$B4_sre)
+# lo rifaccio in un par 2X2 cambiando per ogni plot la palette di colori
+par(mfrow=c(2,2))
+clb <- colorRampPalette(c("dark blue","blue","light blue"))(100)
+clg <- colorRampPalette(c("dark green","green","light green"))(100)
+clr <- colorRampPalette(c("dark red","red","pink"))(100)
+clnir <- colorRampPalette(c("red","orange","yellow"))(100)
+plot(p224r63_2011$B1_sre,col=clb)
+plot(p224r63_2011$B2_sre,col=clg)
+plot(p224r63_2011$B3_sre,col=clr)
+plot(p224r63_2011$B4_sre,col=clnir)
+ 
 
