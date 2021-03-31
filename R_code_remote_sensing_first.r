919dd3e673857cd87b516e6144a3f04a7b75f463
ErnPug <80513465+ErnPug@users.noreply.github.com>
2021-03-31 02:43:03 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -59,5 +59,38 @@ plot(p224r63_2011$B1_sre,col=clb)
 plot(p224r63_2011$B2_sre,col=clg)
 plot(p224r63_2011$B3_sre,col=clr)
 plot(p224r63_2011$B4_sre,col=clnir)
+
+# Day 5
+# Utilizzo di RGB plotting per la visualizzazione dei dati
+# Bande Landsat
+# B1: blu
+# B2: verde
+# B3: rosso
+# B4: infrarosso vicino
+# B5: infrarosso medio
+# B6: infrarosso termico
+# B7: infrarosso medio
+# Ad ogni banda di landsat associamo una compenente dell'RGB. B=>1, G=>2, R=>3. 
+plotRGB(p224r63_2011,r=3,g=2,b=1,stretch="Lin") #uso "stretch" per plottare tutti i valori di riflettanza.
+# Modifico le bande usare per il plot RGB
+plotRGB(p224r63_2011,r=4,g=3,b=2,stretch="Lin")
+plotRGB(p224r63_2011,r=3,g=4,b=2,stretch="Lin") # la banda 4 è l'infrarosso vicino, con riflettanza alta nelle piante.
+plotRGB(p224r63_2011,r=3,g=2,b=4,stretch="Lin")
+
+# Inseriamo in un multiframe i diversi plotRGB
+pdf("primo_pdf_con_R.pdf") # Salviamo in pdf il grafico e salvarlo nella cartella "lab"
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011,r=3,g=2,b=1,stretch="Lin")
+plotRGB(p224r63_2011,r=4,g=3,b=2,stretch="Lin")
+plotRGB(p224r63_2011,r=3,g=4,b=2,stretch="Lin")
+plotRGB(p224r63_2011,r=3,g=2,b=4,stretch="Lin")
+dev.off()
+# cambiamo tipo di stretch, "hist"
+plotRGB(p224r63_2011,r=3,g=4,b=2,stretch="hist")
+# usando la funzione par, inseriamo tre polot in un unico plot
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011,r=3,g=2,b=1,stretch="Lin") # primo caso
+plotRGB(p224r63_2011,r=3,g=4,b=2,stretch="Lin") # infrarosso nel Green
+plotRGB(p224r63_2011,r=3,g=4,b=2,stretch="hist") # stretch diverso
  
 
