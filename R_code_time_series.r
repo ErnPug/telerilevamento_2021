6b9cc52268290e216351168fa92edc68315e98f7
ErnPug <80513465+ErnPug@users.noreply.github.com>
2021-04-08 20:45:00 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -2,4 +2,9 @@
 # Grerenland increase of temperature
 # Data and code from Emanuela Cosma
 
+# install.packages("raster")
+library(raster) # richiamo il pacchetto installato 
+setwd("C:/lab/Groenlandia") # imposto come working directory la cartella "Groenlandia" in "lab" ed in C
+
+
 
