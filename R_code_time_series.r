fdf15192b49a6c2c0317c15c531b387febfe23ae
ErnPug <80513465+ErnPug@users.noreply.github.com>
2021-04-10 16:35:43 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -3,7 +3,9 @@
 # Data and code from Emanuela Cosma
 
 # install.packages("raster")
-library(raster) # richiamo il pacchetto installato 
+library(raster) # richiamo il pacchetto "raster"
+library(rasterVis) # richiamo il pacchetto "rasterVis"
+library(knitr) # richiamo il pacchetto "knitr"
 setwd("C:/lab/Groenlandia") # imposto come working directory la cartella "Groenlandia" in "lab" ed in C
 
 # carico un unico file usando la function "raster"
@@ -14,7 +16,7 @@ plot(lst_2005)
 lst_2010 <- raster("lst_2010.tif")
 plot(lst_2010)
 lst_2015 <- raster("lst_2015.tif")
-plot(lst_2015)ù
+plot(lst_2015)
 # raggruppo tutto in un'unica immagine 
 par(mfrow=c(2,2))
 plot(lst_2000)
@@ -35,6 +37,37 @@ plotRGB(TGr,1,2,3,stretch="Lin")
 # usiamo adesso anche il 2015, elimenando il 2000
 plotRGB(TGr,2,3,4,stretch="Lin")
 
+
+# uso la funzione "levelplot", che usa un'unica leggenda per tutte le immagini
+levelplot(TGr)
+# uso levellot per un silngolo strato
+levelplot(TGr$lst_2000)
+
+# cambiamo la palette di colori
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+# la sezione per vambiare la palette in "levelplot" è "col.regions"
+levelplot(TGr,col.regions=cl)
+# nomino i vari layer dell'immagine 
+levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+# metto anche il nome
+levelplot(TGr,col.regions=cl, main="LST variation in time", 
+          names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+
+# creiamo una lista con i file melt scaricati nella cartella "Groenlandia"
+meltlist <- list.files(pattern="melt")
+# applichiamo a tutti i file la function "raster"
+import2 <- lapply(meltlist,raster)
+# impacchetto tutto
+melt <- stack(import2)
+# plotto i miei dati con "levelplot"
+levelplot(melt)
+
+# per vedere la differenza tra i due periodi, facciamo una sottrazione tra i due layer
+melt_amount <- melt$X2007annual_melt - melt$X1979annual_melt # usiamo $ perché sono layers di un file
+# cambio la palette di colori
+clb <- colorRampPalette(c("blue","white","red"))(100)
+plot(melt_amount,col=clb)
+levelplot(melt_amount,col.regions=clb)
 
 
 
