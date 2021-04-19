f16ffe0be9f0dbc164f4fd148c3cd516803fe87c
ErnPug <80513465+ErnPug@users.noreply.github.com>
2021-04-19 15:25:47 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -2,3 +2,9 @@
 
 setwd("C:/lab/")
 library(knitr)
+library(tinytex)
+# tinytex::install_tinytex()
+    
+tinytex::tlmgr_update()
+
+stitch("R_code_Greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
