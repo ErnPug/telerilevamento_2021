215e75347f2419b4219201381cfe86c8d34314c8
ErnPug <80513465+ErnPug@users.noreply.github.com>
2021-04-24 23:44:39 +0200

    Create R_code_classification.r

---
+++ R_code_classification.r
@@ -0,0 +1,20 @@
+# R_code_classification.r
+library(raster)
+library(RStoolbox)
+setwd("C:/lab/") # sistemo la working directiry
+
+so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+so
+# visualizzo l'immagine
+plotRGB(so,1,2,3,stretch="lin")
+
+# unsupervised classification
+soc <- unsuperClass(so, nClas=3)
+
+# la funzione ha creato in output diversi elementi, tra cui la mappa, che dovremmo plottare.
+
+plot(soc$map)
+
+# prova con 20 classi
+soc20 <- unsuperClass(so, nClass=20)
+plot(soc20$map)
