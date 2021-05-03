2dd249395f5000848c0c36a01ea19475ec102e20
ErnPug <80513465+ErnPug@users.noreply.github.com>
2021-05-03 17:12:45 +0200

    Create R_code_vegetation_indices.r

---
+++ R_code_vegetation_indices.r
@@ -0,0 +1,14 @@
+# R_code_vegetation_indices.r
+
+library(raster)
+setwd(C:/lab/")
+# carico le immagini della deforazione
+defor1 <- brick("defor1.jpg")
+defor2 <- brick("defor2.jpg")
+# plotto le due immagini
+# b1=NIR, b2=red, b3=green
+par(mfrow=c(2,1))
+plotRGB(defor1,r=1,g=2,b=3,stretch="lin")
+plotRGB(defor2,r=1,g=2,b=3,stretch="lin")
+
+
