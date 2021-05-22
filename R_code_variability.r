c2f48151f8efb5391075fb0585245db55b8cb4a9
ErnPug <80513465+ErnPug@users.noreply.github.com>
2021-05-22 20:09:57 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -26,4 +26,18 @@
  # faccio la stessa cosa con la media 
  ndvimean3 <- focal(ndvi,w=matrix(1/9,nrow=3,ncol=3),fun=mean)
  plot(ndvimean3, col=clsd)
+ # aumento il numero della griglia 13X13
+ ndvisd13 <- focal(ndvi,w=matrix(1/169,nrow=13,ncol=13),fun=sd)
+ plot(ndvisd13, col=clsd)
+ # l'ideale sarebbe avere 5X5
+ ndvisd5 <- focal(ndvi,w=matrix(1/25,nrow=5,ncol=5),fun=sd)
+ plot(ndvisd5, col=clsd)
+ # adesso calcolo la PCA e poi usero la moving qindow per il calcolo della deviazione standard
+ # PCA
+ sentpca <- rasterPCA(sent)
+ summary(sentpca$model)
+ # la prima PC contiene il 67.36804 dell'informazione originale
+
+
+
  
