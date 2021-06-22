a4693377590f25f802b9f0b95d229964b11cabca
ErnPug <80513465+ErnPug@users.noreply.github.com>
2021-06-22 17:42:08 +0200

    Update R_file_project.r

--- R_file_project.r
+++ R_file_project.r
@@ -1,6 +1,7 @@
 # R_file_project.r
 
 library(raster)
+library(ncdf4)
 setwd("C:/lab/Progetto/")
 
 miss2006 <- brick("20060512missr.jpeg")
@@ -8,3 +9,5 @@ miss2011 <- brick("20110510missr.jpeg")
 par(mfrow=c(2,1))
 plotRGB(miss2006,r=1,g=2,b=3,stretch="Lin")
 plotRGB(miss2011,r=1,g=2,b=3,stretch="Lin")
+
+prova <- brick("c_gls_WB100_202105010000_GLOBE_S2_V1.0.1.nc")
