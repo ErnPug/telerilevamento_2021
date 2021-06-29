6d12160744d6e835af32da98299190426861715a
ErnPug <80513465+ErnPug@users.noreply.github.com>
2021-06-29 18:08:08 +0200

    Update R_file_project.r

--- R_file_project.r
+++ R_file_project.r
@@ -2,30 +2,46 @@
 # NON ANCORA COMPLETATO
 
 library(raster)
-library(ncdf4)
-setwd("C:/lab/Nilo/")
+library(RStoolbox)
+library(ggplot2)
+library(gridExtra)
 
-rlist2020 <- list.files(pattern="20201025")
-rlist2013 <- list.files(pattern="20200912")
+setwd("C:/lab/Nilo/Step1")
 
-import2020 <- lapply(rlist2020,raster)
-import2013 <- lapply(rlist2013,raster)
+# BANDE 
+# BANDA 1 Coastal/Aerosol
+# BANDA 2 Blue
+# BANDA 3 Green
+# BANDA 4 Red 
+# BANDA 5 Near Infrared
+# BANDA 6 Short Wavelength Infrared
+# BANDA 7 Short Wavelength Infrared
+# BANDA 8 Panchromatic
+# BANDA 9 Cirrus
 
-TGr2020 <- stack(import2020)
-TGr2013 <- stack(import2013)
+# STEP 1 Studio dell'oasi più grande del deserto occidentale, al-Fayyum.
+# carico le due immagini. Creo una lista raccogliendo tutte le bande di ogni immagine.
+step12020 <- list.files(pattern="20201025")
+step12013 <- list.files(pattern="20200912")
 
-plot(TGr2013)
-plot(TGr2020)
+# applico ad ogni immagine della lista la funzione "raster"
+importstep12020 <- lapply(step12020,raster)
+importstep12013 <- lapply(step12013,raster)
 
+# unisco in un unico file
+TGrstep12020 <- stack(importstep12020)
+TGrstep12013 <- stack(importstep12013)
 
-nilo2013 <- brick("20060512missr.jpeg")
-nilo2020 <- brick("20110510missr.jpeg")
-
-
-
-
+# Rispetto ai vari layer nell'immagine, la banda 1 è nel terzo layer, la 2 nel quarto e così via.
+# Raggruppo le immagini. Uso plotRGB per plottarle.
 par(mfrow=c(2,1))
-plotRGB(miss2006,r=1,g=2,b=3,stretch="Lin")
-plotRGB(miss2011,r=1,g=2,b=3,stretch="Lin")
+plotRGB(TGr2013,r=7,g=6,b=5,stretch="lin")
+plotRGB(TGr2020,r=7,g=6,b=5,stretch="lin")
 
-prova <- brick("c_gls_WB100_202105010000_GLOBE_S2_V1.0.1.nc")
+# provo il plottaggio con ggplot
+p2020 <- ggRGB(TGr2020,r=7,g=6,b=5,stretch="LIN")
+p2013 <- ggRGB(TGr2013,r=7,g=6,b=5,stretch="LIN")
+grid.arrange(p2013,p2020,nrow=2)
+
+setwd("C:/lab/Nilo/Step1")
+
