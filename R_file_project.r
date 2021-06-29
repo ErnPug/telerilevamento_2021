62c4cd5a0928f4eefc99b1bb2a53c064e54e9fb6
ErnPug <80513465+ErnPug@users.noreply.github.com>
2021-06-29 20:41:36 +0200

    Update R_file_project.r

--- R_file_project.r
+++ R_file_project.r
@@ -5,6 +5,8 @@ library(raster)
 library(RStoolbox)
 library(ggplot2)
 library(gridExtra)
+library(rasterVis) # richiamo il pacchetto "rasterVis"
+library(knitr) # richiamo il pacchetto "knitr"
 
 setwd("C:/lab/Nilo/Step1")
 
@@ -21,27 +23,73 @@ setwd("C:/lab/Nilo/Step1")
 
 # STEP 1 Studio dell'oasi più grande del deserto occidentale, al-Fayyum.
 # carico le due immagini. Creo una lista raccogliendo tutte le bande di ogni immagine.
-step12020 <- list.files(pattern="20201025")
+step12021 <- list.files(pattern="20210615")
 step12013 <- list.files(pattern="20200912")
 
 # applico ad ogni immagine della lista la funzione "raster"
-importstep12020 <- lapply(step12020,raster)
+importstep12021 <- lapply(step12021,raster)
 importstep12013 <- lapply(step12013,raster)
 
 # unisco in un unico file
-TGrstep12020 <- stack(importstep12020)
+TGrstep12021 <- stack(importstep12021)
 TGrstep12013 <- stack(importstep12013)
 
 # Rispetto ai vari layer nell'immagine, la banda 1 è nel terzo layer, la 2 nel quarto e così via.
 # Raggruppo le immagini. Uso plotRGB per plottarle.
 par(mfrow=c(2,1))
-plotRGB(TGr2013,r=7,g=6,b=5,stretch="lin")
-plotRGB(TGr2020,r=7,g=6,b=5,stretch="lin")
+plotRGB(TGrstep12013,r=7,g=6,b=5,stretch="lin")
+plotRGB(TGrstep12021,r=7,g=6,b=5,stretch="lin")
 
 # provo il plottaggio con ggplot
-p2020 <- ggRGB(TGr2020,r=7,g=6,b=5,stretch="LIN")
-p2013 <- ggRGB(TGr2013,r=7,g=6,b=5,stretch="LIN")
-grid.arrange(p2013,p2020,nrow=2)
+p2021 <- ggRGB(TGrstep12021,r=7,g=6,b=5,stretch="LIN")
+p2013 <- ggRGB(TGrstep12013,r=7,g=6,b=5,stretch="LIN")
+grid.arrange(p2013,p2021,nrow=2)
+
+# Calcolo il DVI delle due immagini
+# Metto le bande del rosso e del NIR in due oggetti, dato che non riesco a prenderlo direttamente da TGRstep1
+# Anno 2013
+NIR2013 <- raster("LC08_L2SP_176040_20130413_20200912_02_T1_SR_B5.TIF")
+RED2013 <- raster("LC08_L2SP_176040_20130413_20200912_02_T1_SR_B4.TIF")
+
+DVI2013 <- NIR2013 - RED2013
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifico la palette di colori
+
+
+# Anno 2021
+NIR2021 <- raster("LC08_L2SP_176040_20210606_20210615_02_T1_SR_B5.TIF")
+RED2021 <- raster("LC08_L2SP_176040_20210606_20210615_02_T1_SR_B4.TIF")
+
+DVI2021 <- (NIR2021 - RED2021)
+
+# Plotto i risultati
+par(mfrow=c(2,1))
+plot(DVI2013, col=cl)
+plot(DVI2021, col=cl)
+
+# Faccio la differenza tra i risultati
+DIFDVI <- DVI2013 - DVI2021
+cld <- colorRampPalette(c('blue','white','red'))(100)
+plot(DIFDVI, col=cld)
+
+# Calcolo adesso NDVI
+# 2013
+NDVI2013 <- DVI2013 / (RED2013 + NIR2013)
+plot(NDVI2013, col=cl)
+
+#2021
+NDVI2021 <- DVI2021 / (RED2021 + NIR2021)
+plot(NDVI2021, col=cl)
+
+# Faccio la differenza dei due NDVI
+DIFFNDVI <- NDVI2013 - NDVI2021
+plot(DIFFNDVI, col=cld)
+
+# facciamo una unsupervised classification, classificazione effettuata dal software, per capire quanti pixel hanno un NDVI positivo, quanti negativo.
+soc <- unsuperClass(DIFFNDVI, nClas=2)
+plot(soc$map)
+freq(soc$map)
+# Zona con DIFFNDVI positivo 1  1681744
+# Zona con DIFFNDVI negativo 3   872752
 
 setwd("C:/lab/Nilo/Step1")
 
